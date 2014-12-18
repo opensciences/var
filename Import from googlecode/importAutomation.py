efedc8dfc9af72c47915fba3da0c03f7b0c062cc
Carter Pape <carterpape@gmail.com>
2014-12-18 15:01:36 -0500

    Random catches

--- Import from googlecode/importAutomation.py
+++ Import from googlecode/importAutomation.py
@@ -1,12 +1,26 @@
-relativePath = "issues/"
+"""
+Exceptions:
+mccabehalsted: there are triple curly brackets ({{{) in jm1 that jekyll doesn't like
+spe: is in "other" directory in terapromise
+reuse: double curly brackets in reuse
+dump: links end like "/ABACUS2013" without closing slash
+"""
+
+
+relativePath = "defect/ck/"
 
 import os, re, datetime
+from types import NoneType
 
 def extractSummary(fileContents):
     return re.search("^#summary ([^\n]+)\n", fileContents).group(1)
 
 def extractAuthor(fileContents):
-    return re.search(r"\|\| Donated by \[?[^ ]* ?([^\]]+)\]? \|\|", fileContents).group(1)
+    results = re.search(r"\|\| Donated by (\[[^ ]* )?([^\]|]+)\]? \|\|", fileContents)
+    if type(results.group(2)) == NoneType:
+        return results.group(1)
+    else:
+        return results.group(2)
 
 def genHeader(baseName, fileContents):
     summary = extractSummary(fileContents)
@@ -30,18 +44,26 @@ def reformatLinks(fileContents):
     return re.sub(r"\[([^ ]+) ([^\]]+)\]", r"[\2](\1)", sub)
 
 def changeURLs(fileContents, relativePath):
-    sub = re.sub("http://promisedata.googlecode.com/svn/trunk/[^/]+/", "https://terapromise.csc.ncsu.edu:8443/svn/repo/" + relativePath, fileContents)
-    return re.sub("http://code.google.com/p/promisedata/source/browse/trunk/[^/]+/", "https://terapromise.csc.ncsu.edu:8443/svn/repo/" + relativePath, sub)
+    hasHiddenParentQ = (type(re.search(r"\d$", baseName)) != NoneType) and (relativePath == "defect/mccabehalsted/")
+    teraPromiseRelativePath = relativePath + baseName
+    if hasHiddenParentQ:
+        teraPromiseRelativePath = relativePath + baseName[:-1] + "/" + baseName
+    sub = re.sub("http://promisedata.googlecode.com/svn/trunk/[^/]+/(" + baseName + "/)?", "https://terapromise.csc.ncsu.edu:8443/svn/repo/" + teraPromiseRelativePath + r"/", fileContents)
+    return re.sub("http://code.google.com/p/promisedata/source/browse/trunk/[^/]+/(" + baseName + "/)?", "https://terapromise.csc.ncsu.edu:8443/svn/repo/" + teraPromiseRelativePath + r"/", sub)
 
 def removeExtraneousLinks(fileContents):
     return fileContents
 
 def reformatTables(fileContents):
-    sub = re.sub(r"When \|\| What", r"When \| What\r---- | ----", fileContents)
+    sub = re.sub(r"\|\| When \|\| What \|\|", r"When | What\r---- | ----", fileContents)
     return re.sub(r"\|\| ([^|]+) \|\| ([^|]+) \|\|", r"\1 | \2", sub)
 
+def escapeCurlyBrackets(fileContents):
+    sub = re.sub(r"{", r"\{", fileContents)
+    return re.sub(r"}", r"\}", sub)
+
 def extractDateString(fileContents):
-    result = re.search(r"\n\|\| *([^ |]+ [^ |]+ [^ |]+) *\|\| Donated by[^|]+\|\|\n", fileContents).group(1)
+    result = re.search(r"\n\|\| *([^ |]+ [^ |]+ [^ |]+) *\|\| Donated by[^|]+\|\|", fileContents).group(1)
     return result
 
 def dateAddedString(fileContents):
@@ -74,6 +96,7 @@ for subdir, dirs, files in os.walk(directory):
         fileContents = changeURLs(fileContents, relativePath)
         fileContents = removeExtraneousLinks(fileContents)
         fileContents = reformatTables(fileContents)
+        fileContents = escapeCurlyBrackets(fileContents)
 
         writeObj = file(newFilePath, "w")
         writeObj.write(header + fileContents)
